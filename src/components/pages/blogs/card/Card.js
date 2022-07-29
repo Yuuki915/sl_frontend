d1bef783b99fadbbe7418bd1fde73db08ec5569b
Yuuki915 <aso091335@gmail.com>
2022-07-28 23:07:51 -0700

    modify

---
+++ src/components/pages/blogs/card/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import format from "date-fns/format";
+
+export default function Card({ blog }) {
+  return (
+    <div className="card">
+      <Link key={blog._id} to={`/blogs/${blog.slug}`} className="read-btn">
+        <div className="box-img">
+          {/* <img src={`/uploads/imgs/${blog.img} `} alt="" /> */}
+        </div>
+        <div className="blog-container">
+          <div className="blog">
+            <div className="box-top">
+              <div className="blog-title">{blog.title}</div>
+            </div>
+
+            <div className="box-bottom">
+              <div>
+                <div className="blog-author">@{blog.author}</div>
+                <div className="blog-created-time">
+                  {format(new Date(blog && blog.createdAt), "MM.dd.yyyy")}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
