5e6d6de70ea9f73e306a83182cb775a757380d84
Yuuki915 <aso091335@gmail.com>
2022-08-05 23:42:35 -0700

    add hamburger, modify category

--- src/components/pages/blogs/CategoryPage.js
+++ src/components/pages/blogs/CategoryPage.js
@@ -1,15 +1,18 @@
+import "../../../css/pages/CategoryPage.css";
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { useBlogsContext } from "../../../hooks/useBlogsContext";
+import { useAuthContext } from "../../../hooks/useAuthContext";
+
 import Card from "../../partials/card/Card";
 import Categories from "../../partials/Categories";
+import Hamburger from "../../partials/Hamburger";
 import Footer from "../../partials/Footer";
-
-import "../../../css/pages/CategoryPage.css";
 
 export default function CategoryPage() {
   const { blogs } = useBlogsContext();
   const params = useParams();
+  const { user } = useAuthContext();
 
   const catBlogs =
     blogs &&
@@ -18,34 +21,50 @@ export default function CategoryPage() {
     });
 
   return (
-    <div className="category-page">
-      <div className="sidebar">
-        <Link to="/blogs" className="to-top">
-          Sharelog
-        </Link>
-        <div className="categories-wrapper">
-          <p className="show-current-category">{params.category}</p>
-          <div className="cat-options">
-            <h4>Categories</h4>
-            <Categories />
+    <div className="cat-page-wrapper">
+      <Hamburger />
+      <div className="category-page">
+        <div className="sidebar">
+          <Link to="/blogs" className="to-top">
+            Sharelog
+          </Link>
+          <div className="categories-wrapper">
+            <p className="show-current-category">{params.category}</p>
+            <div className="cat-options">
+              <h4>Categories</h4>
+              <Categories />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="blog-wrapper">
-        <div className="bb-posi">
-          <button>
-            <Link to="/blogs/new" className="btn addmore-btn">
-              Add Blog
-            </Link>
-          </button>
+        <div className="blog-wrapper">
+          <div className="bb-posi">
+            <button>
+              <Link to="/blogs" className="back-btn">
+                Top page
+              </Link>
+            </button>
+            {user ? (
+              <button>
+                <Link to="/blogs/new" className="btn addmore-btn">
+                  Add Blog
+                </Link>
+              </button>
+            ) : (
+              <></>
+            )}
+          </div>
+          {catBlogs.length > 1 ? (
+            <div className="blog-cards">
+              {catBlogs &&
+                catBlogs.map((blog) => <Card key={blog._id} blog={blog} />)}
+            </div>
+          ) : (
+            <div className="blog-cards">No blogs yet</div>
+          )}
         </div>
-        <div className="blog-cards">
-          {catBlogs &&
-            catBlogs.map((blog) => <Card key={blog._id} blog={blog} />)}
-        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
---
+++ src/components/partials/Hamburger.js
@@ -0,0 +1,115 @@
+import "../../css/Hamburger.css";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useLogout } from "../../hooks/useLogout";
+
+import { IoMdClose } from "react-icons/io";
+import { BiUserCircle } from "react-icons/bi";
+
+import Categories from "./Categories";
+
+export default function Hamburger() {
+  const [hamClass, setHamClass] = useState(false);
+  const [logoutClass, setLogoutClass] = useState(false);
+
+  const { logout } = useLogout();
+
+  const { user } = useAuthContext();
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  const openHandler = () => {
+    setHamClass(true);
+  };
+  const closeHandler = () => {
+    setHamClass(false);
+    setLogoutClass(false);
+  };
+
+  const [count, setCount] = useState(1);
+  const showLogout = () => {
+    setCount(count + 1);
+    if (count % 2 === 0) {
+      setLogoutClass(false);
+      return setCount(1);
+    } else {
+      setLogoutClass(true);
+    }
+  };
+
+  return (
+    <div className="hamburger-wrapper">
+      <div className="user-lines">
+        {user && user ? (
+          <div>
+            <div className="user-icon-name" onClick={showLogout}>
+              <BiUserCircle className="user-icon" />
+              <p>{user && user.username}</p>
+            </div>
+
+            <div className={`${logoutClass ? "" : "logput-btn-none"}`}>
+              <div className="logout-btn-arrow"></div>
+              <Link to="/blogs" className="logout-btn" onClick={handleLogout}>
+                <p className="logout-text">Logout</p>
+              </Link>
+              <p
+                className={`${logoutClass ? "logout-clear-bg" : ""}`}
+                onClick={closeHandler}
+              ></p>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+        <div className="lines" onClick={openHandler}>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+
+      <div
+        className={`${hamClass ? "ham-dark" : ""}`}
+        onClick={closeHandler}
+      ></div>
+      <div className={`${hamClass ? "hamburger" : "hamburger ham-toggle"}`}>
+        <div className="icon">
+          <IoMdClose className="close-icon" onClick={closeHandler} />
+        </div>
+
+        {user && <h2 className="ham-username">Hi, {user && user.username}</h2>}
+
+        <div className="cats">
+          <h4>Categories</h4>
+          <Categories />
+        </div>
+        <div className="ham-user">
+          {user && (
+            <div className="ham-user">
+              <Link
+                to="/blogs"
+                className="ham-auth-link ham-logout-btn"
+                onClick={handleLogout}
+              >
+                Logout
+              </Link>
+            </div>
+          )}
+          {!user && (
+            <div className="ham-user">
+              <Link to="/user/login" className="ham-auth-link">
+                Login
+              </Link>
+              <Link to="/user/register" className="ham-auth-link">
+                Register
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
