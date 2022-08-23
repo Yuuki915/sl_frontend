faa30f2c5efb1a01dd4cae8ef1b610123b89f165
Yuuki915 <aso091335@gmail.com>
2022-08-23 11:00:22 -0700

    made map an array in Blog.js

--- src/components/pages/blogs/Blogs.js
+++ src/components/pages/blogs/Blogs.js
@@ -6,7 +6,8 @@ export default function Blogs({ blogs }) {
     <div className="blogs-wrapper">
       <p className="all-blogs">All Blogs</p>
       <div className="blogs">
-        {blogs && blogs.map((blog) => <Card key={blog._id} blog={blog} />)}
+        {Array.isArray(blogs) &&
+          blogs.map((blog) => <Card key={blog._id} blog={blog} />)}
       </div>
     </div>
   );
