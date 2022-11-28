acfb0f3b73abea22be5eeb531a0b5426ef956b45
Yuuki915 <aso091335@gmail.com>
2022-11-28 15:01:02 -0800

    modify css, home

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ function App() {
       <Router>
         <ScrollTop />
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
           <Route path="/user/login" element={<Login />} />
           <Route path="/user/register" element={<Register />} />
 
-          <Route path="/" element={<Main />} />
+          <Route path="/blogs" element={<Main />} />
           <Route path="/blogs/category/:category" element={<CategoryPage />} />
           <Route path="/blogs/new" element={<NewPost />} />
           <Route path="/blogs/:slug" element={<Show />} />
